3352e991d5c173209cca03d1ef7d01b620679c50
Vincelogs <vincentharyor@gmail.com>
2023-11-08 12:17:06 +0000

    added to the requirements

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -12,7 +12,59 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
+if getenv('AUTH_TYPE') == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif getenv('AUTH_TYPE') == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
+
+@app.before_request
+def filter_request():
+    """filter request
+    """
+    if auth is None:
+        return
+
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """unauthorized request handler
+
+    Args:
+        error (error): error object
+    """
+    return jsonify({'error': 'Unauthorized'}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """handles request where user is authenticated
+    but not allowed to access a resource
+
+    Args:
+        error (error): 403 forbidden
+
+    Returns:
+        str: status message and code
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 @app.errorhandler(404)
 def not_found(error) -> str:

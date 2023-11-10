3e94dde88b18dc9475a8eede36ab19d068d0b02c
Vincelogs <vincentharyor@gmail.com>
2023-11-10 11:18:16 +0000

    class to manage the API authentication.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,11 +7,11 @@ from typing import List, TypeVar
 class Auth:
     """class for handling auth"""
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        return False
+     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+         return True
 
-    def authorization_header(self, request=None) -> str:
-        return None
+     def authorization_header(self, request=None) -> str:
+         return request.headers.get('Authorization')
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        return None
+     def current_user(self, request=None) -> TypeVar('User'):
+         return None

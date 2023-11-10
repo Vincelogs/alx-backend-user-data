81838744cc7489234e75678c8ac30d47eeae1898
Vincelogs <vincentharyor@gmail.com>
2023-11-10 11:41:53 +0000

    Update the method def require_auth(self, path: str, excluded_paths: List[str]) -> bool:

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,6 +18,36 @@ class Auth:
         Returns:
             bool:
         """
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+
+        # handle * at end of excluded paths
+        if path[-1] == '/':
+            path = path[:-1]
+
+        contains_slash = False
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '/':
+                excluded_path = excluded_path[:-1]
+                contains_slash = True
+
+            if excluded_path.endswith('*'):
+                idx_after_last_slash = excluded_path.rfind('/') + 1
+                excluded = excluded_path[idx_after_last_slash:-1]
+
+                idx_after_last_slash = path.rfind('/') + 1
+                tmp_path = path[idx_after_last_slash:]
+
+                if excluded in tmp_path:
+                    return False
+
+            if contains_slash:
+                contains_slash = False
+
+        path += '/'
+
+        if path in excluded_paths:
+            return False
 
         return True
 
